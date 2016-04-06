fa09086fbb1e1126e55d522fa0c461a873b725f8
ConnorNG <netco_nessa@hotmail.com>
2016-04-05 23:55:58 -0600

    removed the encoding crap

--- zjk910FinalProject/controller/modifyController.js
+++ zjk910FinalProject/controller/modifyController.js
@@ -24,11 +24,11 @@ var modifyApp =angular.module('modifyApp', [])
 				if (release_year != "")
 				{
 					address = address +"/"+release_year;
-					var json = {'game_title': encodeURI(document.getElementById("gameTitle").value),
-						'release_year': encodeURI(document.getElementById("gameReleaseYear").value),
-						'publisher' : encodeURI(document.getElementById("gamePublisher").value),
-						'developer' : encodeURI(document.getElementById("gameDeveloper").value),
-						'budget': encodeURI(document.getElementById("gameBudget").value) };
+					var json = {'game_title': (document.getElementById("gameTitle").value),
+						'release_year': (document.getElementById("gameReleaseYear").value),
+						'publisher' : (document.getElementById("gamePublisher").value),
+						'developer' : (document.getElementById("gameDeveloper").value),
+						'budget': (document.getElementById("gameBudget").value) };
 					$http.put(address,json);
 				}
 			}
@@ -56,9 +56,9 @@ var modifyApp =angular.module('modifyApp', [])
 					if (game_system != "")
 					{
 						address = address +"/"+game_system;
-						var json = {'game_title': encodeURI(document.getElementById("gameSystemGameTitle").value),
-						'release_year': encodeURI(document.getElementById("gameSystemGameReleaseYear").value),
-						'system': encodeURI(document.getElementById("gameSystem").value)};
+						var json = {'game_title': (document.getElementById("gameSystemGameTitle").value),
+						'release_year': (document.getElementById("gameSystemGameReleaseYear").value),
+						'system': (document.getElementById("gameSystem").value)};
 						$http.put(address,json);
 					}
 				}
@@ -86,9 +86,9 @@ var modifyApp =angular.module('modifyApp', [])
 					if (game_region != "")
 					{
 						address = address +"/"+game_region;
-						var json = {'game_title': title,
-						'release_year': release_year,
-						'region': game_region};
+						var json = {'game_title': (document.getElementById("gameRegionGameTitle").value),
+						'release_year':  (document.getElementById("gameRegionGameReleaseYear").value),
+						'region': (document.getElementById("gameRegion").value)};
 
 						$http.put(address,json);
 					}
@@ -116,9 +116,9 @@ var modifyApp =angular.module('modifyApp', [])
 					if (game_tag != "")
 					{
 						address = address +"/"+game_tag;
-						var json = {'game_title': encodeURI(document.getElementById("gameTagGameTitle").value),
-						'release_year': encodeURI(document.getElementById("gameTagGameReleaseYear").value),
-						'tag': encodeURI(document.getElementById("gameTag").value)};
+						var json = {'game_title': (document.getElementById("gameTagGameTitle").value),
+						'release_year': (document.getElementById("gameTagGameReleaseYear").value),
+						'tag': (document.getElementById("gameTag").value)};
 						$http.put(address,json);
 					}
 				}
@@ -139,10 +139,10 @@ var modifyApp =angular.module('modifyApp', [])
 				console.log("here");
 				address = address +"/"+dev;
 				console.log(address);
-				var json = {'publisher':encodeURI(document.getElementById("publisherName").value),
-						'founding_year': encodeURI(document.getElementById("publisherFoundingYear").value),
-						'final_year': encodeURI(document.getElementById("publisherFinalYear").value),
-						'hq_region':encodeURI(document.getElementById("publisherHQ").value)};
+				var json = {'publisher':(document.getElementById("publisherName").value),
+						'founding_year': (document.getElementById("publisherFoundingYear").value),
+						'final_year': (document.getElementById("publisherFinalYear").value),
+						'hq_region':(document.getElementById("publisherHQ").value)};
 
 				$http.put(address,json);
 			}
@@ -161,24 +161,24 @@ var modifyApp =angular.module('modifyApp', [])
 				console.log("here");
 				address = address +"/"+dev;
 				console.log(address);
-				var json = {'developer':encodeURI(document.getElementById("developerName").value),
-						'founding_year': encodeURI(document.getElementById("developerFoundingYear").value),
-						'final_year': encodeURI(document.getElementById("developerFinalYear").value),
-						'hq_region':encodeURI(document.getElementById("developerHQ").value)};
+				var json = {'developer':(document.getElementById("developerName").value),
+						'founding_year': (document.getElementById("developerFoundingYear").value),
+						'final_year': (document.getElementById("developerFinalYear").value),
+						'hq_region':(document.getElementById("developerHQ").value)};
 
 				$http.put(address,json);
 			}
 		}
 
 		$scope.gameSale =function logSales(){
-			var json = {'game_title' : encodeURI(document.getElementById("salesGameTitle").value),
-				'release_year' : encodeURI(document.getElementById("salesGameReleaseYear").value),
-				'price' : encodeURI(document.getElementById("salesPrice").value),
-				'system':encodeURI(document.getElementById("salesSystem").value),
-				'units' :encodeURI(document.getElementById("saleUnits").value),
-				'last_update' : encodeURI(document.getElementById("salesUpdate").value),
-				'organization' : encodeURI(document.getElementById("salesOrginization").value),
-				'region' : encodeURI(document.getElementById("salesRegion").value)};
+			var json = {'game_title' : (document.getElementById("salesGameTitle").value),
+				'release_year' : (document.getElementById("salesGameReleaseYear").value),
+				'price' : (document.getElementById("salesPrice").value),
+				'system':(document.getElementById("salesSystem").value),
+				'units' :(document.getElementById("saleUnits").value),
+				'last_update' : (document.getElementById("salesUpdate").value),
+				'organization' : (document.getElementById("salesOrginization").value),
+				'region' : (document.getElementById("salesRegion").value)};
 			console.log(json);
 
 			var game_title = encodeURI(document.getElementById("salesGameTitle").value);
